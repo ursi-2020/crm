df990ca3edb9b3f4927f20720eedb6419caeb835
Quentin REYNAUD <quentin.reynaud@epita.fr>
2020-02-03 12:49:42 +0100

    create ticket if not exist

--- application/djangoapp/views.py
+++ application/djangoapp/views.py
@@ -192,21 +192,39 @@ Add the source of the ticket
 def update_save_tickets(tickets, src):
     error = False
     for t in tickets['tickets']:
-        if t['client'] != '':
-            try:
-                # Update customer fidelity points
-                if src == 'e-commerce':
-                    customer = Customer.objects.get(Email=t['client'])
-                else:
-                    customer = Customer.objects.get(IdClient=t['client'])
-                customer.Credit = customer.Credit + int(t['prix']) / 2
-                # compute average basket
-                if customer.PanierMoyen != 0:
-                    customer.PanierMoyen = (customer.PanierMoyen + int(t['prix'])) / 2
-                else:
-                    customer.PanierMoyen = int(t['prix'])
-                customer.save()
+        try:
+            Ticket.objects.get(uid=src+str(t['id']))
+        except Ticket.DoesNotExist:
+            if t['client'] != '':
+                try:
+                    # Update customer fidelity points
+                    if src == 'e-commerce':
+                        customer = Customer.objects.get(Email=t['client'])
+                    else:
+                        customer = Customer.objects.get(IdClient=t['client'])
+                    customer.Credit = customer.Credit + int(t['prix']) / 2
+                    # compute average basket
+                    if customer.PanierMoyen != 0:
+                        customer.PanierMoyen = (customer.PanierMoyen + int(t['prix'])) / 2
+                    else:
+                        customer.PanierMoyen = int(t['prix'])
+                    customer.save()
 
+                    # Save the ticket
+                    new_ticket = Ticket(DateTicket=parse_datetime(t['date']), Prix=t['prix'], Client=t['client'],
+                                        PointsFidelite=t['pointsFidelite'], ModePaiement=t['modePaiement'], Origin=src, uid=src+str(t['id']))
+                                        #,CustomerPromo=t['CustomerPromo'], GlobalPromo=t['GlobalPromo'])
+                    new_ticket.save()
+                    if t['articles'] != '':
+                        for article in t['articles']:
+                            new_article = PurchasedArticle(codeProduit=article['codeProduit'],
+                                                           prixAvant=article['prix'], prixApres=article['prixApres'],
+                                                           promo=article['promo'], quantity=article['quantity'],
+                                                           ticket=src+str(t['id']))
+                            new_article.save()
+                except ObjectDoesNotExist:
+                    error = True
+            else :
                 # Save the ticket
                 new_ticket = Ticket(DateTicket=parse_datetime(t['date']), Prix=t['prix'], Client=t['client'],
                                     PointsFidelite=t['pointsFidelite'], ModePaiement=t['modePaiement'], Origin=src, uid=src+str(t['id']))
@@ -219,21 +237,6 @@ def update_save_tickets(tickets, src):
                                                        promo=article['promo'], quantity=article['quantity'],
                                                        ticket=src+str(t['id']))
                         new_article.save()
-            except ObjectDoesNotExist:
-                error = True
-        else :
-            # Save the ticket
-            new_ticket = Ticket(DateTicket=parse_datetime(t['date']), Prix=t['prix'], Client=t['client'],
-                                PointsFidelite=t['pointsFidelite'], ModePaiement=t['modePaiement'], Origin=src, uid=src+str(t['id']))
-                                #,CustomerPromo=t['CustomerPromo'], GlobalPromo=t['GlobalPromo'])
-            new_ticket.save()
-            if t['articles'] != '':
-                for article in t['articles']:
-                    new_article = PurchasedArticle(codeProduit=article['codeProduit'],
-                                                   prixAvant=article['prix'], prixApres=article['prixApres'],
-                                                   promo=article['promo'], quantity=article['quantity'],
-                                                   ticket=src+str(t['id']))
-                    new_article.save()
     if error:
         return JsonResponse({"Error": "Client does not exist"})
     return JsonResponse({"SUCESS": "Fidelity point updated"})
